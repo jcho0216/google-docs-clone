c3bc6000bf41373128cb4150d9b724a69e77a1bc
Jun Joy <chojuneseo1@gmail.com>
2021-05-26 22:23:41 +0900

    google-docs-clone
    google docs clone

---
+++ google-docs-clone/client/src/TextEditor.js
@@ -0,0 +1,19 @@
+import React, {useCallback, useEffect, useRef} from 'react'
+import Quill from 'quill'
+import "quill/dist/quill.snow.css"
+
+export default function TextEditor() {
+    const wrapperRef = useCallback((wrapper) => {
+        console.log(wrapper)
+        if(wrapper == null) return
+        wrapper.innerHTML = ''
+        const editor = document.createElement('div')
+        wrapper.append(editor)
+        new Quill(editor, {theme: "snow"})
+    }, [])
+    return (
+        <div className="container" ref={wrapperRef}>
+            
+        </div>
+    )
+}
